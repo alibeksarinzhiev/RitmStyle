7fecde92c73f97c23f67fb3da9f76041a3a8d0d5
alibek <alibek1024@mail.ru>
2024-08-22 17:04:53 +0600

    alibek commit

---
+++ js.js
@@ -0,0 +1,85 @@
+// let btn = document.querySelector('button')
+
+// function showText(){
+//     console.log('вы кликнули');
+    
+    
+// }
+// function showText2(){
+//     console.log('вы клинкули на 2 функ')
+// }
+
+// btn.onclick = showText2;
+// btn.onclick = showText;
+// btn.addEventListener('click',(e)=>{
+// console.log(e.target)  показывает на какой элемент нажали
+// console.log(e.clientX); показывает положение курсора мыши при нажатии
+// console.log(e.clientY);
+// console.log(e);
+// })
+
+// let input1 =document.querySelector('.first')
+// console.log(input1.value);
+
+// let form = document.querySelector('form')
+// form.addEventListener('submit',(e)=>{
+//     e.preventDefault()
+//     console.log(e.target[0].value);
+// })
+
+
+// btn.addEventListener('click',showText2)
+
+// let box1 = document.querySelector('.box1')
+// let box2 = document.querySelector('.box2')
+// let box3 = document.querySelector('.box3')
+
+// box1.addEventListener('click',(e)=>{
+//     console.log(' событие 1');
+// })
+// box2.addEventListener('click',(e)=>{
+//     console.log(' событие 2');
+// })
+// box3.addEventListener('click',(e)=>{
+//     console.log(' событие 3');
+//     // e.stopPropagation()
+// })
+// box1.addEventListener('click',()=>{})
+
+
+// let btn = document.querySelector('button')
+
+// btn.addEventListener('contextmenu',()=>{
+//     console.log('правая кнопка мыши');
+// })
+// btn.addEventListener('mouseover',()=>{
+//     btn.style.background= 'red'
+// })
+// btn.addEventListener('mouseout',()=>{
+//     btn.style.background = 'black'
+// })
+// btn.addEventListener('keydown',()=>{
+//     console.log('при нажатии на клавишу');
+// })
+// btn.addEventListener('keyup',()=>{
+//     console.log('12445');
+// })
+// btn.addEventListener('keypress',()=>{
+//     console.log('qwrt');
+// })
+
+
+// btn.addEventListener('mousedown',(e)=>{
+//     console.log(e.which);
+// })
+// 1 - левая 
+// 2 - колесо
+// 0 - 3 правая
+
+// document.addEventListener('scroll',(e)=>{
+//     console.log(window.scrollY);
+// })
+// document.addEventListener('keydown',(e)=>{
+//     console.log(e.repeat);
+  
+// })
